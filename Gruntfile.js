a2f4fd492bd70e6745ad3482f9742b240db00099
noemiet <noemiet@uga.edu>
2016-01-17 20:20:33 -0500

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,237 @@
+module.exports = function(grunt) {
+
+  /**
+   * Initialize grunt
+   */
+  grunt.initConfig({
+
+    /**
+     * Read package.json
+     */
+    pkg: grunt.file.readJSON('package.json'),
+
+
+    /**
+     * Set banner
+     */
+    banner: '/**\n' +
+    '<%= pkg.title %> - <%= pkg.version %>\n' +
+    '<%= pkg.homepage %>\n' +
+    'Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
+    'License: <%= pkg.license %>\n' +
+    '*/\n',
+
+
+    /**
+     * Set directory paths
+     */
+    dir: {
+      js: 'js',
+      css: 'css',
+      sass: 'css/sass',
+      img: 'img'
+    },
+
+
+    /**
+     * JSHint
+     * @github.com/gruntjs/grunt-contrib-jshint
+     */
+    jshint: {
+      gruntfile: 'Gruntfile.js',
+      files: ['<%= dir.js %>/src/**/*.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+
+    /**
+     * Concatenate
+     * @github.com/gruntjs/grunt-contrib-concat
+     */
+    concat: {
+      options: {
+        stripBanners: true,
+        banner: '<%= banner %>'
+      },
+      js: {
+        src: '<%= jshint.files %>',
+        dest: '<%= dir.js %>/<%= pkg.name %>.js'
+      },
+    },
+
+
+    /**
+     * Sass compiling
+     * @github.com/gruntjs/grunt-contrib-sass
+     */
+    sass: {
+
+      // Development options
+      dev: {
+        options: {
+          style: 'expanded',
+          // sourcemap: true, // Requires Sass 3.3.0 alpha: `sudo gem install sass --pre`
+          trace: true,
+          debugInfo: true
+        },
+        files: {
+          '<%= dir.css %>/<%= pkg.name %>.css': '<%= dir.sass %>/global.scss'
+        }
+      },
+
+      // Distribution options
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          '<%= dir.css %>/<%= pkg.name %>.css': '<%= dir.sass %>/global.scss'
+        }
+      }
+    },
+
+
+    /**
+     * Minify
+     * @github.com/gruntjs/grunt-contrib-uglify
+     */
+    uglify: {
+
+      // Uglify options
+      options: {
+        banner: '<%= banner %>'
+      },
+
+      // Minify js files in js/src/
+      dist: {
+        src: ['<%= concat.js.dest %>'],
+        dest: '<%= dir.js %>/<%= pkg.name %>.min.js'
+      },
+    },
+
+    browserSync: {
+      bsFiles: ['js/*.js', 'css/*.css', '!**/node_modules/**/*', './*.html'],
+      options: {
+          server: {
+              baseDir: './' // make server from root dir
+          },
+          port: 8000,
+          ui: {
+              port: 8080,
+              weinre: {
+                  port: 9090
+              }
+          },
+          open: false
+      }
+    },
+
+    notify: {
+      watching: {
+        options: {
+          enabled: true,
+          message: 'Watching Files!',
+          title: 'Boil', // defaults to the name in package.json, or will use project directory's name
+          success: true, // whether successful grunt executions should be notified automatically
+          duration: 1, // the duration of notification in seconds, for `notify-send only
+        }
+      },
+
+      server: {
+        options: {
+          enabled: true,
+          message: 'Server Running!',
+          title: 'Materialize',
+          success: true,
+          duration: 1
+        }
+      }
+    },
+
+
+    /**
+     * Watch
+     * @github.com/gruntjs/grunt-contrib-watch
+     */
+    watch: {
+
+      // JShint Gruntfile
+      gruntfile: {
+        files: 'Gruntfile.js',
+        tasks: ['jshint:gruntfile'],
+      },
+
+      // Compile Sass dev on change
+      sass: {
+        files: '<%= dir.sass %>/**/*',
+        tasks: ['sass:dev'],
+      },
+
+      // JShint, concat + uglify JS on change
+      js: {
+        files: '<%= jshint.files %>',
+        tasks: ['jshint', 'concat', 'uglify']
+      },
+
+      // Live reload files
+      livereload: {
+        options: { livereload: true },
+        files: [
+          '<%= dir.css %>/**/*.css',  // all .css files in css/ dir
+          '<%= dir.js %>/**/*.js',    // all .js files in js/ dir
+          '**/*.{html,php}',          // all .html + .php files
+          '<%= dir.img %>/**/*.{png,jpg,jpeg,gif,svg}'  // img files in img/ dir
+        ]
+      }
+    }
+  });
+
+
+  /**
+   * Default Task
+   * run `grunt`
+   */
+  grunt.registerTask('default', [
+    'notify',
+    'jshint',           // JShint
+    'concat:js',        // Concatenate main JS files
+    'uglify',           // Minifiy concatenated JS file
+    'sass:dev',         // Compile Sass with dev settings
+    'browserSync',
+    'watch',
+  ]);
+
+
+  /**
+   * Production tast, use for deploying
+   * run `grunt production`
+   */
+  grunt.registerTask('production', [
+    'jshint',           // JShint
+    'concat:js',        // Concatenate main JS files
+    'uglify',           // Minifiy concatenated JS file
+    'sass:dist',        // Compile Sass with distribution settings
+  ]);
+
+  grunt.registerTask('server', [
+    'browserSync',
+    'notify:server',
+  ]);
+
+
+
+  /**
+   * Load the plugins specified in `package.json`
+   */
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+};
---
+++ js/FrontEndBoilerplate.js
@@ -0,0 +1,10 @@
+/**
+Blank project for front end development - 0.1.0
+http://github.perkinswill.com
+Copyright (c) 2016 Dylan Dechant
+License: MIT
+*/
+// another-script.js for demonstrating file conactenation
+console.log('another-script.js file');
+// main.js
+console.log('main.js file');
