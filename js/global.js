680ee5a3e7d420cd45ef0f00f4af279df0fbe459
noemiet <noemie.tshinanga@perkinswill.com>
2016-05-04 13:55:49 -0400

    Fixed mobile menu

--- js/global.js
+++ js/global.js
@@ -34,6 +34,31 @@ function init_masonry(){
     });
 }
 
+// $(document).ready(function(){
+//     //Init jQuery Masonry layout
+//     init_masonry();
+
+// });
+
+
+// function init_masonry(){
+//     var $container = $('.container-fluid');
+
+//     $container.imagesLoaded( function(){
+//         $container.masonry({
+//           itemSelector: '.grid-item',
+          
+//           isAnimated: true
+//         });
+//     });
+// }
+
+// $('.grid').masonry({
+//   itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
+//   columnWidth: '.grid-sizer',
+//   percentPosition: true
+// });
+
 /*!
  * Masonry v3.3.1
  * Cascading grid layout library
