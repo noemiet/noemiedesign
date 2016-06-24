390ded62ca93ebde650fa3a53779bf53a19981ad
noemiet <noemie.tshinanga@perkinswill.com>
2016-06-24 09:49:39 -0400

    Changes

--- js/global.js
+++ js/global.js
@@ -4933,3 +4933,12 @@ var trim = String.prototype.trim ?
 
 }));
 
+
+$(function() {
+     var pgurl = window.location.href.substr(window.location.href
+.lastIndexOf("/")+1);
+     $(".sidebar ul.list-group li a").each(function(){
+          if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
+          $(this).addClass("active");
+     })
+});
