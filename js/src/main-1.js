f560e3c1d9aadb8e98949f3959882944ae7ef455
noemiet <noemiet@uga.edu>
2017-03-17 01:26:26 -0400

    Menu complete, masonry progress

--- js/src/main-1.js
+++ js/src/main-1.js
@@ -3,41 +3,24 @@ $(document).ready(function(){
     init_masonry();
 
 });
+
 function init_masonry(){
     var $container = $('.masonry-container');
     $container.imagesLoaded( function(){
         $container.masonry({
           columnWidth: '.item',
           itemSelector: '.item'
-          // percentPosition: true,
-          // isAnimated: true
         });
     });
 }
 
 
 
-// $(document).ready(function(){
-//     //Init jQuery Masonry layout
-//     init_masonry();
+$(document).ready(function(){
+    //Init jQuery Masonry layout
+    init_masonry();
 
-// });
+});
 
 
-// function init_masonry(){
-//     var $container = $('.container-fluid');
 
-//     $container.imagesLoaded( function(){
-//         $container.masonry({
-//           itemSelector: '.grid-item',
-          
-//           isAnimated: true
-//         });
-//     });
-// }
-
-// $('.grid').masonry({
-//   itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
-//   columnWidth: '.grid-sizer',
-//   percentPosition: true
-// });
